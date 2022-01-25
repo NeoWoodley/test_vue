11f66056cefdbfeeaf1972add436e4ce845a438e
NeoWoodley <Neo.Woodley@outlook.com>
2022-01-25 22:31:45 +0800

    buildDocker.sh

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # 生成环境包：安装package.json里的包依赖
-yarn install
+node -v
+npm install -g yarn -registry=https://registry.npm.taobao.org
+yarn -v
+#--pure-lockfile  这个参数是在服务器install不生成yarn.lock，防止服务器和本地代码冲突
+yarn install --pure-lockfile
 # 发布包
 yarn build
 # 构建docker镜像：运行Dockerfile文件
