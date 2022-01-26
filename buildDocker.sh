18b8a6319adf559b4510afcb74dd0dbd9a1a3c32
NeoWoodley <Neo.Woodley@outlook.com>
2022-01-26 18:21:38 +0800

    close linting rule

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # 生成环境包：安装package.json里的包依赖
-node -v
-npm install -g yarn -registry=https://registry.npm.taobao.org
-yarn -v
-#--pure-lockfile  这个参数是在服务器install不生成yarn.lock，防止服务器和本地代码冲突
-yarn install --pure-lockfile
+yarn install
 # 发布包
 yarn build
 # 构建docker镜像：运行Dockerfile文件
